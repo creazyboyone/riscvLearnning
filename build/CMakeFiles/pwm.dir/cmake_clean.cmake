b91cb392c6fb3aec0e2c972988bbc9bd0d1499a7
creazyboyone <1020401390@qq.com>
2022-01-05 22:14:20 +0800

    lession 7 timer and lightness!

---
+++ build/CMakeFiles/pwm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pwm.dir/src/pwm/main.c.obj"
+  "CMakeFiles/pwm.dir/src/pwm/main.c.obj.d"
+  "pwm"
+  "pwm.manifest"
+  "pwm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/pwm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
