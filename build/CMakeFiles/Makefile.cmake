9d42265b39387cc1916c5398aa14b6eb1fa5ba90
creazyboyone <1020401390@qq.com>
2022-01-05 21:34:59 +0800

    lession 6 timer and boot break!

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -54,7 +54,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/button.dir/DependInfo.cmake"
+  "CMakeFiles/timer.dir/DependInfo.cmake"
   "lib/CMakeFiles/kendryte.dir/DependInfo.cmake"
   "lib/nncase/CMakeFiles/nncase.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/timer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/timer.dir/src/timer/main.c.obj"
+  "CMakeFiles/timer.dir/src/timer/main.c.obj.d"
+  "timer"
+  "timer.manifest"
+  "timer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/timer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
