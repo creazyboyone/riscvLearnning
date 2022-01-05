ede41b531e7765fa42bcdf7238ec66f2a959d144
creazyboyone <1020401390@qq.com>
2022-01-05 21:22:13 +0800

    lession 5 boot input control rgb!

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -54,7 +54,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/dual_core.dir/DependInfo.cmake"
+  "CMakeFiles/button.dir/DependInfo.cmake"
   "lib/CMakeFiles/kendryte.dir/DependInfo.cmake"
   "lib/nncase/CMakeFiles/nncase.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/button.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/button.dir/src/button/main.c.obj"
+  "CMakeFiles/button.dir/src/button/main.c.obj.d"
+  "button"
+  "button.manifest"
+  "button.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/button.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
