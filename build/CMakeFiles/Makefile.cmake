63eb9c7e566f10db643c046b33c50cf92769b70b
creazyboyone <1020401390@qq.com>
2022-01-09 18:03:19 +0800

    lession 9 rom read and write!

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -54,7 +54,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/keypad.dir/DependInfo.cmake"
+  "CMakeFiles/dmac.dir/DependInfo.cmake"
   "lib/CMakeFiles/kendryte.dir/DependInfo.cmake"
   "lib/nncase/CMakeFiles/nncase.dir/DependInfo.cmake"
   )
---
+++ build/CMakeFiles/dmac.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dmac.dir/src/dmac/main.c.obj"
+  "CMakeFiles/dmac.dir/src/dmac/main.c.obj.d"
+  "dmac"
+  "dmac.manifest"
+  "dmac.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dmac.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
