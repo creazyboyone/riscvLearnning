697622dfd269fac7b2e77ca8af6afe1d25911f94
creazyboyone <1020401390@qq.com>
2022-01-09 18:45:30 +0800

    lession 14 pcm music!

---
+++ build/CMakeFiles/play_pcm.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/play_pcm.dir/src/play_pcm/main.c.obj"
+  "CMakeFiles/play_pcm.dir/src/play_pcm/main.c.obj.d"
+  "play_pcm"
+  "play_pcm.manifest"
+  "play_pcm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/play_pcm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
